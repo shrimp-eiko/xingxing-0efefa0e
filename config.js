a449d491fe7fe10df3d67dbe0458fae3e9edec86
shrimp-eiko <dnk5npfzkt@privaterelay.appleid.com>
2026-09-20 21:53:25 +0800

    Add config.js

---
+++ config.js
@@ -0,0 +1,41 @@
+/*
+ * 给杏杏子的小游戏：个人资料都集中在这里，方便后续替换。
+ */
+window.GAME_CONFIG = {
+  herName: "杏杏子",
+  yourName: "羊羊子",
+  firstLine: "你好，你是虾滑吗",
+  secretMeme: "羊腰子爱闻口臭",
+  cities: [
+    {
+      name: "南京",
+      icon: "🏮",
+      subtitle: "故事开始的地方",
+      memory: "南京那一站，后来想起时，风景已经淡了，你还在。"
+    },
+    {
+      name: "南宁",
+      icon: "🌿",
+      subtitle: "烟火气里的牵手",
+      memory: "南宁的夜很热闹，我却总能先听见你。"
+    },
+    {
+      name: "阳朔",
+      icon: "⛰️",
+      subtitle: "山水都在见证",
+      memory: "阳朔的山水很慢，你回头的那一下更慢。"
+    }
+  ],
+  songTitle: "我们好像在哪见过",
+  bgmSources: [
+    "./assets/music/bgm.mp3",
+    "./assets/music/我们好像在哪见过.mp3",
+    "./assets/music/我们好像在哪见过.m4a"
+  ],
+  photos: [
+    "./assets/photos/1.jpg",
+    "./assets/photos/2.jpg",
+    "./assets/photos/3.jpg",
+    "./assets/photos/4.jpg"
+  ]
+};
